3d1469d4d1f17dcf2bf8cfacf1973108b84152a6
Andrew Matteson <andrew@voreas.io>
2021-06-10 15:55:35 -0500

    Type annotations, clean-up

--- sudoku.py
+++ sudoku.py
@@ -3,22 +3,30 @@
 import os
 import math
 import sys
+from typing import List, Set, Tuple
 
+### Environment Variables ###
+#
+# - SIZE:   board side length; must be a square
+# - OFFSET: minimum symbol value
+#
 SIZE = int(os.environ.get('SIZE', 9))
 if int(math.sqrt(SIZE)) != math.sqrt(SIZE):
     raise ValueError("SIZE must be a square")
 OFFSET = int(os.environ.get('OFFSET', 0))
 
-# 1 through 9
-SYMBOLS = [i + OFFSET for i in range(SIZE)]
+### Types for annotations ###
+Symbol = int
+Cell = Set[Symbol]
+Board = List[List[Set[Symbol]]]
 
-# A cell is just a mutable set of remaining valid symbols.
-# Starts with all symbols.
-def init_cell():
+# Generate the set of valid symbols for this run
+SYMBOLS = list(i + OFFSET for i in range(SIZE))
+
+def init_cell() -> Cell:
     return set(SYMBOLS)
 
-# A board is a cell array of arrays.
-def init_board():
+def init_board() -> Board:
     board = []
     for _ in range(SIZE):
         row_cells = []
@@ -27,8 +35,8 @@ def init_board():
         board.append(row_cells)
     return board
 
-# Returns a string.
-def render_board(board):
+def render_board(board: Board) -> str:
+    '''Returns a string representing the provided board.'''
     cell_dim = int(math.sqrt(SIZE))
     render_dim = (cell_dim + 1) * SIZE
     lines = [[' '] * render_dim for row in range(render_dim)]
@@ -43,8 +51,8 @@ def render_board(board):
                     lines[render_row][render_col] = str(symbol)
     return '\n'.join(''.join(line) for line in lines)
 
-# Update the available symbols for a cell
-def update_cell(board, row, col, symbols):
+def update_cell(board: Board, row: int, col: int, symbols: List[Symbol]):
+    '''Updates the available symbols for a cell.'''
     symbols = set(symbols)
     invalid = symbols - set(SYMBOLS)
     if invalid:
@@ -52,41 +60,48 @@ def update_cell(board, row, col, symbols):
     if row >= SIZE or col >= SIZE:
         raise ValueError("coordinates out of range: ({}, {})".format(row, col))
     board[row][col] = symbols
+    print("\nUPDATE: ({}, {}) -> {}".format(row, col, symbols))
 
-def done(board):
+def done(board: Board) -> bool:
+    '''Returns True if all cells have only one available symbol left'''
     for row in board:
         for cell in row:
             if len(cell) > 1:
                 return False
     return True
 
-def parse_update(line):
+def parse_update(line: str) -> Tuple[int, int, List[Symbol]]:
+    '''Reads a line in the format 'row,col:s1,s2,s3,...' and returns the tuple
+    (row, col, symbols).'''
     coords, symbols = line.split(':')
     row, col = list(map(int, coords.split(',')))
     symbols = list(map(int, symbols.split(',')))
     return row, col, symbols
+
+def resolve_board(board):
+    '''Performs a single pass of analysis -> updates'''
+    # TODO: ADD CODE HERE
+    print('Not yet implemented')
 
 def main():
     board = init_board()
     while not done(board):
         print('---')
         print(render_board(board))
-        print("Provide an update in the format 'row,col:s1,s2,s3,...'")
+        print("Provide a manual update ('row,col:s1,s2,s3,...') or '+' to resolve board")
         for line in sys.stdin:
             line = line.strip()
             if line == 'q':
                 return
-            try:
-                row, col, symbols = parse_update(line.strip())
+            elif line == '+':
+                resolve_board(board)
                 break
-            except:
-                print("Failed to parse update: '{}'".format(line))
-        print()
-        try:
-            update_cell(board, row, col, symbols)
-            print("Applied update ({}, {}) -> {}".format(row, col, symbols))
-        except Exception as e:
-            print("Failed to apply update: '{}': {}".format(line, e))
-
+            else:
+                try:
+                    row, col, symbols = parse_update(line.strip())
+                    update_cell(board, row, col, symbols)
+                    break
+                except Exception as e:
+                    print("ERROR: failed to apply update: '{}': {}".format(line, e))
 
 main()
